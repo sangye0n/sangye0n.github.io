807eea4a396bd66414d98ee060476365e36be5f5
sangye0n <70354951+sangye0n@users.noreply.github.com>
2020-10-23 12:27:05 +1100

    update 1

---
+++ imagenet/assets/js/main.js
@@ -0,0 +1,101 @@
+$(document).ready(function(){
+	
+
+
+	$('.colorbtn-home').click(function(){
+		$('.description').removeClass('hide')
+		$('.item').removeClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').adClass('normal');
+		$('.about').addClass('hide')
+
+	}); 
+
+	$('.colorbtn.clear').click(function(){
+		$('.item').addClass('hide');
+		$('.item.clear').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('clear');
+		$('.about').addClass('hide')
+
+
+	});
+
+	$('.colorbtn.red').click(function(){
+		$('.item').addClass('hide');
+		$('.item.red').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('red');
+		$('.about').addClass('hide')
+
+	});
+
+	$('.colorbtn.yellow').click(function(){
+		$('.item').addClass('hide');
+		$('.item.yellow').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('yellow');
+		$('.about').addClass('hide')
+
+	});
+
+	$('.colorbtn.green').click(function(){
+		$('.item').addClass('hide');
+		$('.item.green').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('green');
+		$('.about').addClass('hide')
+
+	});
+
+	$('.colorbtn.ivory').click(function(){
+		$('.item').addClass('hide');
+		$('.item.ivory').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('ivory');
+		$('.about').addClass('hide')
+
+	});
+
+	$('.colorbtn.pink').click(function(){
+		$('.item').addClass('hide');
+		$('.item.pink').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('pink');
+		$('.about').addClass('hide')
+
+	});
+
+	$('.colorbtn.ivory').click(function(){
+		$('.item').addClass('hide');
+		$('.item.ivory').removeClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('ivory');
+		$('.about').addClass('hide')
+
+
+	});
+	$('.colorbtn-about').click(function(){
+		$('.item').addClass('hide');
+		$('.description').addClass('hide');
+		$('.leftbox').removeClass('red yellow green clear normal ivory pink');
+		$('.leftbox').addClass('normal');
+		$('.about').removeClass('appear hide')
+		$('.about').addClass('appear')
+
+	});
+
+});
+
+
+
+
+
+
